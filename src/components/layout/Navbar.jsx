1ba3e98b5011ddcf92a92f13ff5a7b47cb1ad2c6
Rayaan Khan <rayaan.k.ca@gmail.com>
2023-10-03 08:22:23 -0400

    footer

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -14,6 +14,7 @@ function Navbar({title}) {
 
         <div className="flex-1 px-2 mx-2">
           <div className="flex justify-end">
+            
             <Link to="/" className='btn btn-ghost btn-sm rounded-btn'>
               Home
             </Link>
@@ -24,8 +25,6 @@ function Navbar({title}) {
 
           </div>
         </div>
-
-
     </div>
   </nav>
 }
